2e6c9bf709e7650976fce1f69fbdf39704df7fba
mtakahiro <mtaka@astro.ucla.edu>
2015-11-03 15:04:16 -0800

    Done

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,1 +1,13 @@
 import numpy as np
+import math
+
+nmax = 10
+fn = np.zeros(nmax,dtype="int")
+for i in range(1,nmax,1):
+    if i < 2:
+        fn[i] = 1
+    else:
+        fn[i] += fn[i-1] + fn[i-2]
+
+print fn
+
