0a36c50600db3cbc5e9fc9e83c5473fd769ad72a
albertfxwang <albertfxwang@gmail.com>
2015-11-03 16:03:41 -0800

    done task 2

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import numpy as np
 
-a = int(raw_input('Give the number of the element in fib: '))
+a = int(raw_input('Give the number of the element in fib (within 1000): '))
 
 def fib(n):
     a, b = 0, 1
@@ -9,8 +10,16 @@ def fib(n):
         yield a
         a, b = b, a + b
 
-fiblist = list(fib(100))
+fiblist = list(fib(1000))
 fiblist.pop(0)
 
 print 'the element is ', fiblist[int(a-1)]
 
+
+b = float(raw_input('Give me the number: '))
+
+fibarray = np.asarray(fiblist)
+c = fibarray - b
+ind = np.argmin(np.abs(c))
+
+print 'the closest element is ', fiblist[ind]
